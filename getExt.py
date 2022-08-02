b89d0b46f66dd6a95ff6058d9e91adba4dc79af5
Abdallah Eldesouky <52884095+abdallah0x01@users.noreply.github.com>
2022-08-02 15:40:20 +0200

    Update getExt.py

--- getExt.py
+++ getExt.py
@@ -1,6 +1,8 @@
+# this script get the extensions of files 
 import os
+path = input('Enter the path: ').strip()
 extensions = []
-for root, directories, filenames in os.walk(os.getcwd()):
+for root, directories, filenames in os.walk(path):
     for file in filenames:
         firstpart, ext = os.path.splitext(file)
         if ext not in extensions:
