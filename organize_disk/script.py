5432832fb5dc2f7cf69ae118f372f664f46aaf76
Abdallah Eldesouky <52884095+abdallah0x01@users.noreply.github.com>
2022-08-08 14:21:03 +0200

    Update script.py

--- organize_disk/script.py
+++ organize_disk/script.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 # path = input('Enter path you want organize files in: ').strip()
+extensions = ['txt','jpg','jpeg','png','mp4','mp3','html','css','iso','pdf','docx','exe','py','sh','c']
 path = 'c:\\users\\abdallah\\desktop'
 os.chdir(path)
 path2copy2 = 'c:\\users\\abdallah\\desktop\\dir'
@@ -8,15 +9,17 @@ path2copy2 = 'c:\\users\\abdallah\\desktop\\dir'
 for root, dirnames,filenames in os.walk(path):
     for filename in filenames:
         firstpart, ext = os.path.splitext(filename)
-        if not os.path.exists(f'{path2copy2}\\{ext}'):
-            os.makedirs(f'{path2copy2}\\{ext}')
-            try:
-                shutil.copy(f'{root}\\{filename}',f'{path2copy2}\\{ext}')
-            except:
-                continue
+        ext = ext.strip('.')
+        if ext in extensions:
+            if not os.path.exists(f'{path2copy2}\\{ext}'):
+                os.makedirs(f'{path2copy2}\\{ext}')
+                try:
+                    shutil.copy(f'{root}\\{filename}',f'{path2copy2}\\{ext}')
+                except:
+                    continue
 
-        else:
-            try:
-                shutil.copy(f'{root}\\{filename}',f'{path2copy2}\\{ext}')
-            except:
-                continue
+            else:
+                try:
+                    shutil.copy(f'{root}\\{filename}',f'{path2copy2}\\{ext}')
+                except:
+                    continue
